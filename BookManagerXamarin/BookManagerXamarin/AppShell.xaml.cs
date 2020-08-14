d8eed954bb208e73beefb0c97f4d5c63711d9ca3
tagkuya <fortis931taku@gmail.com>
2020-08-14 11:53:00 +0900

    Initial Commit

---
+++ BookManagerXamarin/BookManagerXamarin/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using BookManagerXamarin.ViewModels;
+using BookManagerXamarin.Views;
+using Xamarin.Forms;
+
+namespace BookManagerXamarin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
